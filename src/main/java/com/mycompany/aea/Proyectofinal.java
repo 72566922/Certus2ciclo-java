7ce8e64ebe9aa0aa99d3672b8ce000a1614c233e
kevin <129197277+72566922@users.noreply.github.com>
2023-12-25 16:34:09 -0500

    Colegio forms

---
+++ src/main/java/com/mycompany/aea/Proyectofinal.java
@@ -0,0 +1,18 @@
+package com.mycompany.aea;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+
+
+/**
+ *
+ * @author Morales
+ */
+public class Proyectofinal {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
